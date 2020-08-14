358e907caa7fa2f42e7da0ee6e553b2da5a01d8d
KIL Bismuth <kiralowrey@gmail.com>
2020-08-13 19:00:23 -0700

    add button and clear button finished

--- assets/groclist.js
+++ assets/groclist.js
@@ -1,28 +1,24 @@
 // JavaScript for groclist.html specific elements
-// VARIABLES FIRST
-const listItem = 
+
+// FUNCTIONS
+
+// BUTTON ELEMENTS  
+// add a list element
+$("#add-list-item").on("click", function () {
+  const listItem = 
   $(`<div class="row list-item">
       <div class="input-field">
         <input type="checkbox" />
         <input placeholder="Grocery Item" type="text" class="validate">
       </div>
     </div>`);
-
-// FUNCTIONS
-
-// BUTTON ELEMENTS  
-// add a list element
-function addListItem () {
   $('#groc-list').append(listItem);
-  console.log($('#groc-list'));
-}
-/*$("#add-list-item").on("click", function () {
-  $('#groc-list').append(listItem);
-  // this button only works once. gonna look at a todo list repository. I think I'm missing something big -KIL
-});*/
+});
 // remove checked items
 $("#clear-check-items").on("click", function () {
-  // if input type="checkbox" is also "checked" then we remove it's <div class="row list-item" and everything inside
+  let inputField = $('.input-field');
+  let checkboxes = inputField.children('input:checked');
+  checkboxes.parent().parent().remove();
 });
 // save list changes
 $("#save-list").on("click", function () {
